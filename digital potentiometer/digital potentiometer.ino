b4178957e8e643c3586d5f791fd115934bc11fd4
anandamoyi <anandamoyi_saha@hotmail.com>
2022-11-05 11:25:58 -0400

    Update digital potentiometer.ino

--- digital potentiometer/digital potentiometer.ino
+++ digital potentiometer/digital potentiometer.ino
@@ -17,49 +17,53 @@ float minVo;
 float maxVo;
 void setup()
 {
-pinMode (CS , INPUT) ; // initialising 10 pin as output pin
+//pinMode (CS , INPUT) ; // initialising 10 pin as output pin
 pinMode (A0, OUTPUT) ; // initialising pin A0 as input pin
-pinMode(A1, INPUT);
-SPI.begin() ; // this begins Serial peripheral interfece
+//pinMode(A1, INPUT);
+//SPI.begin() ; // this begins Serial peripheral interfece
 Serial.begin(9600) ; // this begins serial communications between arduino and ic.
 }
 void loop()
 {
-for (int i = 0; i <= 255; i++)// this run loops from 0 to 255 step with 10 ms delay between each step
-{
-digitalPotWrite(i) ; // this writes level i to ic which determines resistance of ic
- Ra = (Rab*(256-i))/256;
- Rb = (Rab*i)/256;
-// Serial.print("i= ") ;
-// Serial.println(i) ;
- Serial.print("Ra= ") ;
- Serial.println(Ra) ;
- Serial.print("Rb= ") ;
- Serial.println(Rb) ;
-// minVo = minVin*(1+(Rb/Ra));
-// maxVo = maxVin*(1+(Rb/Ra));
-delay(10);
+// for (int i = 0; i <= 255; i++)// this run loops from 0 to 255 step with 10 ms delay between each step
+// {
+// digitalPotWrite(i) ; // this writes level i to ic which determines resistance of ic
+//  Ra = (Rab*(256-i))/256;
+//  Rb = (Rab*i)/256;
+// // Serial.print("i= ") ;
+// // Serial.println(i) ;
+//  Serial.print("Ra= ") ;
+//  Serial.println(Ra) ;
+//  Serial.print("Rb= ") ;
+//  Serial.println(Rb) ;
+// // minVo = minVin*(1+(Rb/Ra));
+// // maxVo = maxVin*(1+(Rb/Ra));
+// delay(10);
+// rawVo = analogRead(A0) ; // read analog values from pin A0
+// Vo = rawVo*(5.0/1023.0);
+// rawVin = analogRead(A1) ; // read analog values from pin A0
+// Vin = rawVin*(5.0/1023.0);
+// //Vin = analogRead(A1) ;
+// //Voltage = x; // this converts the analog value to corresponding voltage level
+// //Serial.print("Level i = " ) ; // these serial commands print value of i or level and voltage across wiper
+// //Serial.print(i) ; // and gnd on Serial monitor of arduino IDE
+// //Serial.print("\t Value = ") ;
+// //Serial.print("Vin= ") ;
+// //Serial.println(Vin) ;
+// Serial.println() ;
+// //Serial.print("Vin= ") ;
+// //Serial.println(Vin) ;
+// Serial.print("Vo= ") ;
+// Serial.println(Vo) ;
+// Serial.println() ;
+// delay(1000);
+
+// }
+delay(500);
 rawVo = analogRead(A0) ; // read analog values from pin A0
 Vo = rawVo*(5.0/1023.0);
-rawVin = analogRead(A1) ; // read analog values from pin A0
-Vin = rawVin*(5.0/1023.0);
-//Vin = analogRead(A1) ;
-//Voltage = x; // this converts the analog value to corresponding voltage level
-//Serial.print("Level i = " ) ; // these serial commands print value of i or level and voltage across wiper
-//Serial.print(i) ; // and gnd on Serial monitor of arduino IDE
-//Serial.print("\t Value = ") ;
-//Serial.print("Vin= ") ;
-//Serial.println(Vin) ;
-Serial.println() ;
-//Serial.print("Vin= ") ;
-//Serial.println(Vin) ;
 Serial.print("Vo= ") ;
 Serial.println(Vo) ;
-Serial.println() ;
-delay(1000);
-
-}
-delay(500);
 // for (int i = 255; i >= 0; i--) // this run loops from 255 to 0 step with 10 ms delay between each step
 // {
 // digitalPotWrite(i) ;
