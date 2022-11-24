4acc7c7047fa8474b6e89475a3b5f08ff2b8f402
Rania Ouassif <rania.ouassif@mail.mcgill.ca>
2022-11-24 12:10:52 -0500

    CORRECT CODE!!!!

--- digipot--NEW/digipot--NEW.ino
+++ digipot--NEW/digipot--NEW.ino
@@ -1,30 +1,33 @@
 #include <SPI.h>
 
-byte address = 0x00;
+int address = 0x00;
 int CS= 10;
 
-void setup() {
-  pinMode (CS, OUTPUT);
-  SPI.begin();
+void setup()
+{
+pinMode (CS, OUTPUT);
+SPI.begin();
 }
 
-void loop(){
-  for (int i = 0; i <= 128; i++){
-  digitalPotWrite(i);
-  delay(10);
-  }
-  delay(500);
-  for (int i = 128; i >= 0; i--) {
-  digitalPotWrite(i);
-  delay(10);
-  }
+void loop()
+{
+for (int i = 0; i <= 128; i++)
+{
+digitalPotWrite(i);
+delay(10);
+}
+delay(500);
+for (int i = 128; i >= 0; i--)
+{
+digitalPotWrite(i);
+delay(10);
+}
 }
 
-int digitalPotWrite(int value) {
-  digitalWrite(CS, LOW);
-  delay(100);
-  SPI.transfer(address);
-  SPI.transfer(value);
-  delay(100);
-  digitalWrite(CS, HIGH);
+int digitalPotWrite(int value)
+{
+digitalWrite(CS, LOW);
+SPI.transfer(address);
+SPI.transfer(value);
+digitalWrite(CS, HIGH);
 }
